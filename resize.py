da2e613327fdf0624edc939457288029064c18ad
Takuya-Shuto-engineer <shuto.takuya.sy@alumni.tsukuba.ac.jp>
2020-08-05 00:31:04 +0900

    Modification of resize.py

--- resize.py
+++ resize.py
@@ -2,9 +2,15 @@ from PIL import Image
 import sys
 import os
 import re
+import argparse
 
-input_path = "datasets/mgan-dataset/"
-output_path = "datasets/resized_images/"
+parser = argparse.ArgumentParser(description='Resize images.')
+parser.add_argument('--input_path', type=str, default="datasets/mgan-dataset/", help='Path to a dataset.')
+parser.add_argument('--output_path', type=str, default="datasets/resized_images/", help='Path to resized images.')
+args = parser.parse_args()
+
+input_path = args.input_path
+output_path = args.output_path
 files = os.listdir(input_path)
 count = 1
 
@@ -17,10 +23,10 @@ for file in files:
     background = Image.new("RGB", image.size, (255, 255, 255))
     background.paste(image, mask=image.split()[3])
     background = background.resize((64, 64))
-    background.save(output_path + os.path.splitext(file)[0] + ".jpg") 
+    background.save(os.path.join(output_path,  os.path.splitext(file)[0] + ".jpg"))
   elif file[-4:] == ".jpg":
     # jpgの時は読み込んでそのままリサイズすればいい
     image = Image.open(os.path.join(input_path, file))
     image = image.resize((64, 64))
-    image.save(output_path + os.path.splitext(file)[0] + ".jpg")
+    image.save(os.path.join(output_path, os.path.splitext(file)[0] + ".jpg"))
     count = count + 1
